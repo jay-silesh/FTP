04ad71231da87cb35d5f250ba6df3bf8a98213df
jayanth <sileshsu@usc.edu>
2013-09-11 02:15:59 -0700

    cleaned up the code

--- working_code/server.cc
+++ working_code/server.cc
@@ -40,7 +40,7 @@ struct sockaddr_in  cli_addr_global;
 
 
 
- void sig_handler(int signo)
+void sig_handler(int signo)
 {
  if (signo == SIGINT)
  fclose(fd);
@@ -111,9 +111,7 @@ void *func_sock(void * portno)
     int counter=0;
     while(1)
     {
- 
-         
-		buffer =(char *)calloc(sizeof(char)*(PACKETSIZE), 1);
+        buffer =(char *)calloc(sizeof(char)*(PACKETSIZE), 1);
         int n=udp_socket2.recieve_packet_through_socket(buffer,PACKETSIZE);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         int x,sequence_number;
         memcpy(&x, buffer, sizeof(int));
@@ -220,147 +218,108 @@ int main(int argc, char *argv[])
 
 
 
+
     pthread_mutex_init(&file_mutex,NULL);
-	if(pthread_create(&secondsock, NULL, func_sock, (void *)&port1)) {
-
+	if(pthread_create(&secondsock, NULL, func_sock, (void *)&port1)) 
+    {
         fprintf(stderr, "Error creating thread\n");
         return 1;
-
-        }
-        if(pthread_create(&thirdsock, NULL, func_sock, (void *)&port2)) {
-
+    }
+    
+    if(pthread_create(&thirdsock, NULL, func_sock, (void *)&port2))
+    {
         fprintf(stderr, "Error creating thread\n");
         return 1;
+    }
 
-	}
-    if(pthread_create(&fourthsock, NULL, func_sock, (void *)&port3)) {
-
+    if(pthread_create(&fourthsock, NULL, func_sock, (void *)&port3))
+    {
         fprintf(stderr, "Error creating thread\n");
         return 1;
-
-        }
-        if(pthread_create(&fifthsock, NULL, func_sock, (void *)&port4)) {
-
+    }
+    if(pthread_create(&fifthsock, NULL, func_sock, (void *)&port4)) 
+    {
         fprintf(stderr, "Error creating thread\n");
         return 1;
-
     }
 	total_packets = get_total_packets();
 	
 	gettimeofday(&start, NULL);
 
    
-   
-   
-    
-    
-    
-	if(pthread_create(&filewrite, NULL, file_write, NULL)) {
+   if(pthread_create(&filewrite, NULL, file_write, NULL)) {
 
 	fprintf(stderr, "Error creating thread\n");
 	return 1;
 
 	} 
 	
-    
-    int sockfd, portno;
     pid_t pid;
     int last_packet_received = 0; //last inorder received
     
     int sequence_number;
-    socklen_t clilen;
-    char *buffer;
     
-    struct sockaddr_in serv_addr,serv_addr_1, cli_addr;
-    int n;
     signal(SIGCHLD,SIG_IGN);
     if (argc < 2) {
  
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {   
-         
-        error("ERROR opening socket");
- 
-    }
      
-    
-    
-    bzero((char*)&serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-     
-        error("ERROR on binding");
-    clilen = sizeof(cli_addr);
-    
+    udp_socket_listener udp_socket3(atoi(argv[1]));    
+        
     int counter=0;
     while(1)
     {
- 
-         
-		buffer =(char *)calloc(sizeof(char)*(PACKETSIZE), 1);
-        n = recvfrom(sockfd, buffer, PACKETSIZE, 0,(struct sockaddr*) &cli_addr, &clilen);
-        if (n < 0)
-        error("ERROR on recvfrom");
-     
-	int x;
+        char *buffer =(char *)calloc(sizeof(char)*(PACKETSIZE), 1);
+        int n=udp_socket3.recieve_packet_through_socket(buffer,PACKETSIZE);
+        int x;
 		memcpy(&x, buffer, sizeof(int));
-    		sequence_number=ntohl(x);
-    		pthread_mutex_lock(&file_mutex);
-    		if(sequence_map.find(sequence_number)==sequence_map.end())
-    		{
-    			
-    			sequence_map.insert(pair<int,char*>(sequence_number,buffer));
-    			
-    		}
-    		else
-    		{
-    			free(buffer);
-    		}
+    	sequence_number=ntohl(x);
+    	
+    	pthread_mutex_lock(&file_mutex);
+		if(sequence_map.find(sequence_number)==sequence_map.end())
+		{
+			
+			sequence_map.insert(pair<int,char*>(sequence_number,buffer));
+			
+		}
+		else
+		{
+			free(buffer);
+		}
 		pthread_mutex_unlock(&file_mutex);
-
-		       
-            
-            counter = counter + n; 
- 
-        }
+        counter = counter + n; 
+    }
  
     
     printf("%d bytes received\n",counter);
-    close(sockfd);
-    if(pthread_join(filewrite, NULL)) {
+    udp_socket3.close_socket();
 
-	fprintf(stderr, "Error joining thread\n");
-	return 2;
-
+    if(pthread_join(filewrite, NULL))
+    {
+    	fprintf(stderr, "Error joining thread\n");
+    	return 2;
 	}
-	 if(pthread_join(secondsock, NULL)) {
-
-	fprintf(stderr, "Error joining thread\n");
-	return 2;
-
+	if(pthread_join(secondsock, NULL)) 
+    {
+    	fprintf(stderr, "Error joining thread\n");
+    	return 2;
 	}
-	if(pthread_join(thirdsock, NULL)) {
-
-	fprintf(stderr, "Error joining thread\n");
-	return 2;
-
+	if(pthread_join(thirdsock, NULL)) 
+    {
+    	fprintf(stderr, "Error joining thread\n");
+    	return 2;
 	}
-    if(pthread_join(fourthsock, NULL)) {
-
-    fprintf(stderr, "Error joining thread\n");
-    return 2;
-
+    if(pthread_join(fourthsock, NULL)) 
+    {
+        fprintf(stderr, "Error joining thread\n");
+        return 2;
     }
-    if(pthread_join(fifthsock, NULL)) {
-
-    fprintf(stderr, "Error joining thread\n");
-    return 2;
+    if(pthread_join(fifthsock, NULL)) 
+    {
+        fprintf(stderr, "Error joining thread\n");
+        return 2;
     }
 	
 
